1ad8e7487e85db7b1cbc441ee01d522723a303e8
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-16 01:47:21 +0600

    call fake data from file

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,3 +1,4 @@
+/*
 import React, { useEffect, useState } from 'react';
 
 const Shop = () => {
@@ -21,6 +22,30 @@ const Shop = () => {
                             {product.name}
                         </li>
                     )
+                }
+            </ul>
+            
+           
+        </div>
+    );
+};
+
+export default Shop;
+*/
+
+import fakeData from '../../fakeData';
+import React, { useState } from 'react';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    return (
+        <div>
+            <h1>This is Shop</h1>
+            <h3>{products.length}</h3>
+            <ul>    
+                {
+                    products.map(product => <li>{product.name}</li>)
                 }
             </ul>
             
