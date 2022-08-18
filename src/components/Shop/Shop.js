dcdee06c627ec643670204196a820d8dea83b8ca
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-18 12:13:31 +0600

    cart added & flex

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -35,21 +35,25 @@ export default Shop;
 
 import fakeData from '../../fakeData';
 import React, { useState } from 'react';
+import './Shop.css'
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <h3>{products.length}</h3>
-            <ul>    
-                {
-                    products.map(product => <li>{product.name}</li>)
-                }
-            </ul>
-            
-           
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>    
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                <h3>
+                    This is cart
+                </h3>
+            </div>
         </div>
     );
 };
