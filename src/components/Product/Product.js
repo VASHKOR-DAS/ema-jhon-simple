0bc32fee8fd638e68e0fdc64ee1074afb1518331
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-19 00:20:16 +0600

    Products added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import './Product.css'
 
 // kono data parameter hisebe newar jnno 'props' use hoise
 const Product = (props) => {
     // props er moddhe kichu ase kina seta janar jnne, r akhane product ta holo property er naam 
-    
-    //console.log(props.product.name);
+    console.log(props.product);
+
+    const { img, name, seller, price, stock } = props.product //function er parameter er moto kaj kore
     return (
-        <div>
-            {/* <h3>This is Product</h3> */}
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt="product img" srcset="" />
+            </div>
+            <div className="product-name">
+                <h4>{name}</h4>
+                <p><small>by : {seller}</small></p>
+                <br />
+                <p>${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+            </div>
         </div>
     );
 };
