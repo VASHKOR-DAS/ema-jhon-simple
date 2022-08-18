b1a91c3f2a43148a5845b910e039a3fec8c37fcb
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-19 02:27:54 +0600

    handleAddProduct click handler added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 // kono data parameter hisebe newar jnno 'props' use hoise
 const Product = (props) => {
     // props er moddhe kichu ase kina seta janar jnne, r akhane product ta holo property er naam 
-    console.log(props.product);
+    // console.log(props);
 
     const { img, name, seller, price, stock } = props.product //function er parameter er moto kaj kore
     return (
@@ -20,9 +20,11 @@ const Product = (props) => {
                 <p><small>by : {seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
-                <button className="main-button">
-                    <FontAwesomeIcon icon={faShoppingCart}
-                    /> add to cart
+                <button 
+                    className="main-button" 
+                    onClick={ () => (props.handleAddProduct(props.product))} /* function auto execute jeno na hoy, tai er vitore 1ta () => likhte hoy */
+                >
+                        <FontAwesomeIcon icon={faShoppingCart}/> add to cart
                 </button>
             </div>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -39,16 +39,24 @@ import './Shop.css'
 import Product from '../Product/Product';
 
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
+    const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+
+    const handleAddProduct = (product) => {
+        console.log("Product added", product)
+
+    }
+
     return (
         <div className="shop-container">
             <div className="product-container">
-                  
+
                 {
-                    products.map(pd => <Product product={pd}></Product>)
+                    products.map(pd => <Product
+                        handleAddProduct = {handleAddProduct}
+                        product = {pd}></Product>)
                 }
-                
+
             </div>
             <div className="cart-container">
                 <h3>
