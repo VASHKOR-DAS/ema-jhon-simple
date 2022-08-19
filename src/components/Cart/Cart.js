efe283d1360ed04167bb3d6470b4e9872ea1e023
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-19 17:11:08 +0600

    added cart total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,12 +5,33 @@ const Cart = (props) => {
     const cart = props.cart
     // console.log(cart);
 
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    } 
+
+    let items = 0;
+    let tax = 0;
     let total = 0;
+
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
+
+        items = items + product.price;
+        tax =  product.price / 10;
         total = total + product.price;
     }
 
+    let shipping = 0;
+    if (total > 500) {
+        shipping = 0;
+    }
+    else if (total > 200) {
+        shipping = 4.99;
+    }
+    else if (total > 0) {
+        shipping = 12.99
+    }
 
 
     return (
@@ -18,30 +39,28 @@ const Cart = (props) => {
             <h3>Order Summary</h3>
             <p>Items ordered : {cart.length}</p>
 
-            <small>
               <table>
                     <tr>
                         <td>Items :</td>
-                        <td>${}</td>
+                        <td>${formatNumber(items)}</td>
                     </tr>
                     <tr>
                         <td>Shipping & Handling:</td>
-                        <td>${}</td>
+                        <td>${formatNumber(shipping)}</td>
                     </tr>
                     <tr>
                         <td>Total before tax:</td>
-                        <td>${}</td>
+                        <td>${formatNumber(total+shipping)}</td>
                     </tr>
                     <tr>
                         <td>Estimated Tax:</td>
-                        <td>${}</td>
+                        <td>${formatNumber(tax)}</td>
                     </tr>
                     <tr className='total-row'>
                         <td>Order Total:</td>
-                        <td>${}</td>
+                        <td>${formatNumber(total+shipping+tax)}</td>
                     </tr>
                 </table>
-            </small>
 
         </div>
     );
