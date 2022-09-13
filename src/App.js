b1dff6e50a99832e5b2c92eb7bacfc40a11cdc8f
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-13 17:10:52 +0600

    react route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Product from './components/Product/Product';
+// import Product from './components/Product/Product';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+
+      <Router>
+        <Routes>
+          {/* default page show korar jnno */}
+          <Route path="/" element={<Shop></Shop>}></Route>
+
+          <Route path="/shop" element={<Shop></Shop>}></Route>
+
+          <Route exact path="/review" element={<Review></Review>}></Route>
+
+          <Route exact path='/inventory' element={<Inventory></Inventory>}></Route>
+
+          <Route exact path='*' element={<NotFound></NotFound>}></Route>
+
+        </Routes>
+      </Router>
+
     </div>
   );
 }
