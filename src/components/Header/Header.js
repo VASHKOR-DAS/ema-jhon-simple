6964f93b8888847d6783b2cbcf3bdf891b3b0e62
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-16 15:20:24 +0600

    create public folder a file which netlify show properly

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from '../../images/logo.png'; // 1 level-up a hole ../
 import './Header.css' //same folder hole ./
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="logo" srcSet="" />
             <nav>
-
+                    
                     <a href='/shop'>Shop</a>
-                
                     <a href='/review'>Order Review</a>
-
                     <a href='/inventory'>Manage Inventory</a>
                
             </nav>
